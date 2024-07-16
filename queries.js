ed84b680a9aa0d654e6b6503a1e20d83ddea877f
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-07-16 13:32:12 -0500

    added the layout for the queries

---
+++ queries.js
@@ -0,0 +1,18 @@
+const pool = require('pg')
+
+
+
+const pool = new pool(
+{
+user: postgres,
+pasword: 12345,
+host: localhost,
+db: employees_db
+
+
+},
+console.log('connected to database')
+
+);
+
+pool (connect);
